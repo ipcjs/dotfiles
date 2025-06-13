a41428beed5f8982fa7f77865b9b8fa7d95258b8
ipcjs <gipcjs@gmail.com>
2025-06-14 05:29:13 +0800

    fix: 尝试识别非交互模式

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,3 +1,20 @@
+# https://stackoverflow.com/questions/9738535/powershell-test-for-noninteractive-mode
+function IsNonInteractiveShell {
+    # Test each Arg for match of abbreviated '-NonInteractive' command.
+    $NonInteractive = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonI*' }
+
+    if ([Environment]::UserInteractive -and -not $NonInteractive) {
+        # We are in an interactive shell.
+        return $false
+    }
+
+    return $true
+}
+
+if (IsNonInteractiveShell) {
+    return
+}
+
 # Load starship
 Invoke-Expression (&starship init powershell)
 
